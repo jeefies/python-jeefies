a89e5b07f5444b0a1ac1a3bca505032e4141b275
jeefies <jeefy163@163.com>
2020-10-24 00:45:09 +0800

    change init file

--- jeefies/__init__.py
+++ jeefies/__init__.py
@@ -2,9 +2,9 @@ __all__ = ("rand", "strtime", "Context", "Hashsec", "Hexsec",
            "BaseSec", "context", "HyContext")
 from . import rand  # rand num modules
 from .strtime import strtime  # strtime object
-from .content import Context  # Content object
+#from .content import Context  # Content object
 from .jeefies_sec import Hashsec, Hexsec, BaseSec  # encrypt objects
 #from .fibonacci import fibonacci, generate_fibonacci, iterator_fibonacci
 #from .complexity_function import O_test
 import hy as _hy
-from .hy_content import context, Context as HyContext
+from .hy_content import context, Context #using cached context, alse see context by python
